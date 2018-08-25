0c9238f7af4bcffb6b94d73bda15a9f32e42b012
Hyun <shinreality@gmail.com>
2018-08-25 12:17:16 -0400

    Update to Folders

---
+++ controllers/recipes-controller.js
@@ -0,0 +1,25 @@
+var db = require("../models");
+var express = require('express');
+var router = express.Router();
+
+
+router.get("/api/recipes/:id", function (req, res) {
+  // Find one Recipe with the id in req.params.id and return them to the user with res.json
+  db.Author.findOne({
+    where: {
+      id: req.params.id
+    }
+  }).then(function (dbRecipes) {
+    res.json(dbRecipes);
+  });
+});
+
+router.post("/api/recipes", function (req, res) {
+  // Create an Recipe with the data available to us in req.body
+  console.log(req.body);
+  db.Recipes.create(req.body).then(function (dbRecipes) {
+    res.json(dbRecipes);
+  });
+});
+
+module.exports = router;
